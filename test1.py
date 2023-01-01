8eeea77e43ef8e50cdb9c77e2f02de64a15ad0aa
Madhav <madhavagarwal2404@gmail.com>
2023-01-01 11:32:43 +0530

    commit

--- test1.py
+++ test1.py
@@ -1,1 +1,16 @@
-print("hello world")
+import matplotlib.pyplot as plt
+import numpy as np
+Fs = 1e6
+N = 1024
+t = np.arange(100)
+s = np.sin(0.15*2*np.pi*t)
+s = s*np.hamming(100)
+S = np.fft.fft(s)
+S_mag = np.abs(S)
+S_phase = np.angle(S)
+plt.figure(0)
+plt.plot(t,S_mag,'.-')
+plt.figure(1)
+plt.plot(t,S_phase,'.-')
+plt.show()
+
